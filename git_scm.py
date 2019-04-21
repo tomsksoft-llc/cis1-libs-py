9663094883686a58c73e39496145439a87832368
MaksimFelchuck <felchuck@yandex.ru>
2019-04-21 15:02:28 +0700

    modified git_scm.py

--- git_scm.py
+++ git_scm.py
@@ -2,19 +2,17 @@ import sys
 import subprocess
 import os
 
-def repo(rep, commit_id, ref, dir):
+def repo(rep, commit_id, ref, dir, mod_h, mod_b):
     FNULL = open(os.devnull, 'w')
-    if ref != '':
-        
+    if mod_b: 
         subprocess.run('git clone {0} -b {1} {2}'.format(rep,ref,dir))
     else:
-        print('gogo')
         subprocess.run('git clone {0} {1}'.format(rep,dir))
     
     os.chdir(dir)
-    subprocess.run('git rev-parse --is-inside-work-tree')
+    subprocess.run('git rev-parse --is-inside-work-tree', stdout=FNULL)
     
-    if commit_id == '':
+    if not mod_h:
         process = subprocess.Popen('git rev-parse --verify HEAD', stdout=subprocess.PIPE, shell=True)
         output = process.communicate()
         commit_id = output[0].decode('utf8')
@@ -22,62 +20,71 @@ def repo(rep, commit_id, ref, dir):
 
         
     
-    subprocess.run('git reset --hard')
-    subprocess.run('git clean -fdx')
-    subprocess.run('git fetch --tags --progress {0} +refs/heads/*:refs/remotes/origin/*'.format(rep))
-    subprocess.run('git rev-parse {0}'.format(commit_id))
-    subprocess.run('git config core.sparsecheckout')
-    subprocess.run('git checkout -f {0}'.format(commit_id))
-    subprocess.run('git rev-list {0}'.format(commit_id))
-    return True
+    subprocess.run('git reset --hard', stdout=FNULL)
+    subprocess.run('git clean -fdx', stdout=FNULL)
+    subprocess.run('git fetch --tags --progress {0} +refs/heads/*:refs/remotes/origin/*'.format(rep), stdout=FNULL)
+    subprocess.run('git rev-parse {0}'.format(commit_id), stdout=FNULL)
+    subprocess.run('git config core.sparsecheckout', stdout=FNULL)
+    subprocess.run('git checkout -f {0}'.format(commit_id), stdout=FNULL)
+    #subprocess.run('git rev-list {0}'.format(commit_id))
+    return 0
+    
+def usage():
+    print("""
+usage:
 
+git_scm <repo> [-b branch]|[-h commit_hash]<dir>
+
+by default get (pull and checkout) HEAD of the master branch from repo
+
+-b bransh - get head of the specified branch
+-h commit_hash - get the specified revision
+
+Return value:
+
+0 - if success
+non zero - if any error
+""")
 if '__main__':
-    repo(sys.argv[1], sys.argv[2], '', sys.argv[3])
-    """
+    #repo(sys.argv[1], sys.argv[2], '', sys.argv[3])
     if sys.argv[1] == '--help':
-        print('''
-usage:      <-b>  <ref>  <commit_id> <dir>   
-            <link>[<args>]<dir>
-            
-    -b                  Download repository on branch
-    <ref>               Branch name
-    <commit_id>         Commit's id(hash)
-    <dir>               The directory in which the repository will be installed (required) 
-        ''')
+        usage()
+    else:
+        mod_h, mod_b = False, False
+        for arg in range(len(sys.argv)):
+            if sys.argv[arg] == '-h':
+                mod_h = True
+                commit_id = sys.argv[arg+1]
+                sys.argv[arg+1] = None
+            if sys.argv[arg] == '-b':
+                mod_b = True
+                ref = sys.argv[arg+1]
+                sys.argv[arg+1] = None
 
-
-    try:
-        repo = sys.argv[1]
-        if sys.argv[2] == '-b':
-            mode = '-b'
-
-            try:
-                ref = sys.argv[3]
-                dir = sys.argv[4]
-                clone_ref(repo,'', ref, dir)
-            except:
-                print ('fatal:directory or branch not specified')
-
-        else:
-            commit_id = sys.argv[2]
-            dir = sys.argv[3]
-            clone_repo(repo, commit_id,'', dir)
-
-    except:
+        rep = sys.argv[1]
+        dir = sys.argv[-1:]
+        dir = dir[0]
+        
         try:
-            repo = sys.argv[1]
-            dir = sys.argv[2]
-            clone_repo(repo,'','',dir)
-        except:
-            print('''
-usage:      [-b]  <ref>  <commit_id> <dir>   
-            <link>[<args>]<dir>
-
---help to learn more
+            if dir is None:
+                raise Exception("fatal:the last argument should be <dir>")
+            if mod_h and mod_b:
+                res = repo(rep, commit_id, ref, dir, mod_h, mod_b)
+            elif mod_h:
+                res = repo(rep, commit_id, None, dir, mod_h, mod_b)
+            elif mod_b:
+                res = repo(rep, None, ref, dir, mod_h, mod_b)
+            else:
+                res = repo(rep, None, None, dir, mod_h, mod_b)
+            print(res)
+        except Exception as err:
+            print(err)
+            usage()
+            
+            
+            
+            
+            
                 
-        ''')
-"""
+        
 
-
-
-#af696919d867d9d55e618d3307dc6293baa2fb2b
