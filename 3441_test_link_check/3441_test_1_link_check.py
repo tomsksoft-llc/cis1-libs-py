2fbc8093034ac8e0380e0180f2890f49e33407cb
MaksimFelchuck <felchuck@yandex.ru>
2019-05-24 21:14:03 +0700

    edit tests

--- 3441_test_link_check/3441_test_1_link_check.py
+++ 3441_test_link_check/3441_test_1_link_check.py
@@ -3,50 +3,46 @@ import os
 import sys
 from builtins import SystemExit
 
-
+# Run program and return code
 def check(url, depth, external):
-
-
-
-    os.chdir('..')
-    process = subprocess.Popen('.\link_check.py {0} {1} {2}'.format(url,depth,external), stdout=subprocess.PIPE, shell=True)
+    process = subprocess.Popen('python ../link_check.py {0} {1} {2}'.format(url,depth,external), stdout=subprocess.PIPE, shell=True)
     output = process.communicate()
     res = output[0].decode('utf8')
     code = process.poll()
-    os.chdir('3441_test_link_check')
-
-
     return res, code
-
+# If program code = 1 return 1
 def check_code(code):
     if code == 1:
-        print('Program have exceptions')
+        print('link_check.py has an error')
         raise SystemExit(1)
 
 if '__main__':
     url = 'https://deskroll.com/'
-
-    print('Checking depth error...')
+# Depth usage check and check his return code
+    print('Depth usage check...')
     depth_res, depth_code = check(url, 0, 'False')
     check_code(depth_code)
-    print('Depth check result:\n', depth_res)
+    print('Depth usage result:\n', depth_res)
 
-    print('Checking external error...')
+# External usage check and check his return code
+    print('External usage check...')
     external_res, external_code = check(url, 1, 'Folse')
     check_code(external_code)
-    print('External check result:\n', external_res)
-    print('Checking parameters error...')
+    print('External usage result:\n', external_res)
+
+# Parameters usage check and check his return code
+    print('Parameters usage check...')
     parameters_res, parameters_code = check('', '', '')
     check_code(parameters_code)
-    print('Parameters check result:\n', parameters_res)
+    print('Parameters usage check result:\n', parameters_res)
+
+# --help usage check and check his return code
     print('Checking --help usage:')
     help_res, help_code = check('--help', '', '')
     check_code(help_code)
-    print('Help checking  result:\n', help_res)
+    print('--help usage check result:\n', help_res)
 
-
-
-
+# Return codes should include usage and error pointers depending on the type of verification
     correct = True
     if (('list index out of range' in parameters_res) or ('invalid literal for int()' in parameters_res)) and ('usage' in parameters_res):
         print('Parameters check successful')
@@ -69,6 +65,7 @@ if '__main__':
         correct = False
         print('Help check is not correct')
 
+# If at least one return code is not correct return 1
     if correct:
         raise SystemExit(0)
     else:
--- 3441_test_link_check/3441_test_2_link_check.py
+++ 3441_test_link_check/3441_test_2_link_check.py
@@ -3,14 +3,12 @@ import os
 import sys
 from builtins import SystemExit
 
-
+# Run program and return code
 def check(url, depth, external):
-    os.chdir('..')
-    process = subprocess.Popen('.\link_check.py {0} {1} {2}'.format(url,depth,external), stdout=subprocess.PIPE, shell=True)
+    process = subprocess.Popen('python ../link_check.py {0} {1} {2}'.format(url,depth,external), stdout=subprocess.PIPE, shell=True)
     output = process.communicate()
     res = output[0].decode('utf8')
     code = process.poll()
-    os.chdir('3441_test_link_check')
     return res, code
 
 if '__main__':
@@ -19,6 +17,7 @@ if '__main__':
     external = 'False'
     res, code = check(url,depth,external)
     print(res)
+# If program return code = 0 program works correctly
     if code == 1:
         print('Program have exceptions')
         raise SystemExit(1)
