deb0ccda78f95f0c47646133a761547be6d1d839
MaksimFelchuck <felchuck@yandex.ru>
2019-05-28 15:38:50 +0700

    modified and add tests

--- git_scm.py
+++ git_scm.py
@@ -26,8 +26,8 @@ def repo(rep, commit_id, ref, dir, mod_h, mod_b):
     subprocess.run('git rev-parse {0}'.format(commit_id), stdout=FNULL)
     subprocess.run('git config core.sparsecheckout', stdout=FNULL)
     subprocess.run('git checkout -f {0}'.format(commit_id), stdout=FNULL)
-    #subprocess.run('git rev-list {0}'.format(commit_id))
-    return 0
+    
+   
     
 def usage():
     print("""
@@ -46,34 +46,72 @@ Return value:
 non zero - if any error
 """)
 if '__main__':
-    #repo(sys.argv[1], sys.argv[2], '', sys.argv[3])
-    if sys.argv[1] == '--help':
-        usage()
-    else:
-        mod_h, mod_b,commit_id,ref = False, False, False, False
-        for arg in range(len(sys.argv)):
-            if sys.argv[arg] == '-h':
+    try:
+        if sys.argv[1] == '--help':
+            usage()
+            raise sys.exit(0)
+    except Exception as err:
+            print(err)
+            usage()
+            raise sys.exit(0)
+        
+    mod_h, mod_b,commit_id,ref = False, False, False, False
+    dir = sys.argv[-1:]
+    dir = dir[0]
+    
+    for arg in range(len(sys.argv)):
+        if sys.argv[arg] == '-h':
+            try:
                 mod_h = True
-                commit_id = sys.argv[arg+1]
+                commit_id = sys.argv[arg+1]                    
                 sys.argv[arg+1] = None
-            if sys.argv[arg] == '-b':
+            except:
+                print("After argument '-h' must be 'commit_id'")
+                usage()
+                raise sys.exit(0)
+            if commit_id == dir:
+                print("After argument '-h' must be 'commit_id'")
+                usage()
+                raise sys.exit(0) 
+        if sys.argv[arg] == '-b':
+            try:
                 mod_b = True
                 ref = sys.argv[arg+1]
                 sys.argv[arg+1] = None
-
+            except:
+                print("After argument '-b' must be 'ref'")
+                usage()
+                raise sys.exit(0)
+            if ref == dir:
+                print("After argument '-b' must be 'ref'")
+                usage()
+                raise sys.exit(0)
+                    
+                
         rep = sys.argv[1]
-        dir = sys.argv[-1:]
-        dir = dir[0]
         
-        try:
-            if (dir is None) and (dir != rep):
-                raise Exception("fatal:the last argument should be <dir>")
+        args = [
+            rep,
+            mod_h,
+            mod_b,
+            commit_id,
+            ref
+                
+            ]
+        
+        
+        
+        
+    try:
+        if (dir is None) or (dir in args):
+                
+            raise Exception("fatal:the last argument should be <dir>")
             
-            res = repo(rep, commit_id, ref, dir, mod_h, mod_b)
-            print(res)
-        except Exception as err:
-            print(err)
-            usage()
+        repo(rep, commit_id, ref, dir, mod_h, mod_b)
+    except Exception as err:
+        print(err)
+        usage()
+            
             
             
             
