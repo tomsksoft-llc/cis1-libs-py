6b49bb7a4dd817fe9241a98da0bd1efef82d0bb2
Felchuck <felchuck@yandex.ru>
2019-09-13 18:12:51 +0300

    upgrade

--- link_check_v2.py
+++ link_check_v2.py
@@ -1,74 +1,95 @@
-import requests, bs4, webbrowser, re
-from selenium import webdriver
-import urllib.request
-import traceback
 import sys
+import bs4
+import re
+import requests
 
 
 class Link:
-    def __init__(self, valid):
+    def __init__(self, link, valid, parent, status):
         self.valid = valid
+        self.parent = parent
+        self.link = link
+        self.status = status
         pass
 
 
 def start(url, depth, main):
-    print('Start-', url)
-    if not external:
-        external_links, internal_links = get_links(url)
+    external_links, internal_links = get_links(url)
+    if external and external_links:
+        check(internal_links + external_links, url.link, depth - 1, main)
+    elif not external and internal_links:
+        check(internal_links, url.link, depth - 1, main)
+    pass
 
-        print('External links:', external_links)
-        print("Internal links:", internal_links)
-        check(internal_links, depth - 1, main)
 
-
-def check(links, depth, main):
-    for link in links:
-        print(link)
-        print(depth)
+def check(urls, parent_link, depth, main):
+    for link in urls:
         if depth != 0:
-            start(link, depth, False)
+            new_link = Link(link, None, parent_link, None)
+            links.append(new_link)
+            start(new_link, depth, False)
     if main:
         end_status()
-    return
+    pass
 
 
 def end_status():
-    print('END!')
+    print('Web site: {0} has invalid URLs:'.format(main_link.link))
+    for link in links:
+        if not link.valid:
+            print('{0} on parent url {1} ({2})'.format(link.link, link.parent, link.status))
+    print('URLs on {0} checked, all links work.Valid URLs:'.format(main_link.link))
+    for link in links:
+        if link.valid:
+            print(link.link)
 
 
 def get_links(url):
-    res = requests.get(url)
-    if res.status_code != requests.codes.ok:
-        print('{0} is a broken link. Response: 404 Not Found'.format(url))
-    else:
-        print(url, '----OK LINK!')
-    soup = bs4.BeautifulSoup(res.text, "html.parser")
-    # links = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',
-    # res.text)  # список сайтов
-    links = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',
-                       res.text)  # список (протокол, сайт, страница)
-    external = []
-    internal = []
-    for link in links:
-        if url in link:
-            internal.append(link)
-        else:
-            external.append(link)
-    for link in soup.find_all('a', href=True):
-        if (link['href'] not in internal) and (link['href'] not in external):
+    try:
+        result = requests.get(url.link)
+        url.valid = True
+        soup = bs4.BeautifulSoup(res.text, "html.parser")
+        urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',
+                          result.text)
+        external_links = []
+        internal_links = []
+        url.link = url.link.replace('www.', '')
+        for link in urls:
+            link = link.replace('www.', '')
+            check_links.append(link) #проверить
 
-            internal.append(url + link['href'])#не правильная конструкция, на более высокой глубине неккоректные ссылки. создать класс,
-                                                #объекты которого будут содержать в себе ссылку родителя, внутренние ссылки 'logo.php' и т.д записывать в виде
-                                                #"ссылка родитель + внутренняя ссылка" (создавая новый объект класса
+            if link not in check_links:
+                if url.link in link:
+                    internal_links.append(link)
+                else:
+                    external_links.append(link)
+        if not url.parent:
+            url.parent = main_link.link
+        all_links = soup.find_all('a', href=True) + soup.find_all('link', href=True) + soup.find_all('script', src=True) \
+                    + soup.find_all('source', srcset=True)
 
-    return external, internal
-    # res = requests.get(url)
-    # if res.status_code != requests.codes.ok:
+        for href in soup.find_all('a', href=True):
+             #доделать
+            #href = href.replace('www.', '')
+            link = href['href']
 
-    #  print('{0} is a broken link. Response: 404 Not Found'.format(url))
+            if (link not in links) and ((url.parent + link) not in links) and ((main_link.link + link) not in links):
+                if (link not in internal_links) and (link not in external_links):
+
+                    if 'http' not in link:
+                        internal_links.append(url.parent + link)
+                    else:
+                        external_links.append(link)
+
+        return external_links, internal_links
+
+    except Exception as error:
+        url.valid = False
+        url.status = type(error).__name__
+        #print(error)
+        return None, None
 
 
-"""
 def usage():
     print('''
 usage:
@@ -83,22 +104,19 @@ Return value:
 0 - always
 non zero - if any error during start script
 ''')
-    """
+
+
 if '__main__':
-    external = sys.argv[3]
-    depth = int(sys.argv[2])
-    invalid_links, valid_links = [], []
-    if external == 'False':
-        external = False
-    start(sys.argv[1], depth, True)
-    """
-    try:    
+
+    try:
         if sys.argv[1] == '--help':
             usage()
             raise sys.exit(0)
-        url = sys.argv[1]
-        depth = int(sys.argv[2])
-        
+        res = requests.get(sys.argv[1])
+        main_link = Link(sys.argv[1], True, None, None)
+        depth = int(sys.argv[2]) + 1
+        links = []
+        check_links = []
         external = sys.argv[3]
         if external == 'False':
             external = False
@@ -106,14 +124,11 @@ if '__main__':
             external = True
         else:
             raise Exception('External must be True or False')
-        
+
         if depth <= 0:
-            raise Exception('depth must be > 0')
+            raise Exception('Depth must be > 0')
 
-        start(url, depth, external)
-        
-
+        start(main_link, depth, True)
     except Exception as err:
         print(err)
         usage()
-"""
