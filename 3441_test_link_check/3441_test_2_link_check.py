4aa37e15d32bcae460d820717997494b9a222675
felchuck <felchuck@yandex.ru>
2019-10-21 01:51:23 -0700

    edit test2

--- 3441_test_link_check/3441_test_2_link_check.py
+++ 3441_test_link_check/3441_test_2_link_check.py
@@ -1,28 +1,13 @@
-import subprocess
-import os
 import sys
-
-
-# Run program and return code
-def check(url, depth, external):
-    process = subprocess.Popen('python ../link_check.py {0} {1} {2}'.format(url, depth, external),
-                               stdout=subprocess.PIPE, shell=True)
-    output = process.communicate()
-    res = output[0].decode('utf8')
-    code = process.poll()
-    return res, code
-
+sys.path.append('../')
+import lib_test_runner
 
 if '__main__':
     url = 'https://deskroll.com/'
-    depth = 2
-    external = 'False'
-    res, code = check(url, depth, external)
-    print(res)
-    # If program return code = 0 program works correctly
-    if code != 0:
-        print('Program have exceptions')
-        raise sys.exit(1)
+    depth = '1'
+    external = 'False'    
+    res = lib_test_runner.run(['../link_check.py', url, depth, external], "Message for report")
+    if res:
+         lib_test_runner.fail()
     else:
-        print('Program worked correctly')
-        raise sys.exit(0)
+         lib_test_runner.ok()
