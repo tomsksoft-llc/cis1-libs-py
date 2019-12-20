c4096563a314eda8703d049c07cff3d9a4a45858
Dmitriy V. Tsvedel <tdv@tomsksoft.com>
2019-12-20 17:02:16 +0700

    added tests

---
+++ tests/3963_test_redmine/3963_test_1_redmine.py
@@ -0,0 +1,38 @@
+import sys
+
+sys.path.append("../")
+import lib_test_runner
+
+
+def main():
+
+    res = 0
+
+    # invalid option argument
+    if lib_test_runner.run(["../../lib-utils/redmine.py",
+                            "--heRRRprp"], "must be ERROR ") == 0:
+        res = 2
+
+    # --help option
+    if lib_test_runner.run(["../../lib-utils/redmine.py",
+                            "--help"], "must be OK ") != 0:
+        res = 2
+
+    # One invalid arg
+    if lib_test_runner.run(["../../lib-utils/redmine.py",
+                            "New"], "must be ERROR ") == 0:
+        res = 2
+
+    # Both invalid args
+    if lib_test_runner.run(["../../lib-utils/redmine.py",
+                            "invalid_issue", "invalid_status"], "must be ERROR ") == 0:
+        res = 2
+
+    if res == 0:
+        lib_test_runner.test_ok()
+    else:
+        lib_test_runner.test_fail()
+
+
+if __name__ == "__main__":
+    main()
