2a110255d1025f46ed697a58660875fb58683deb
MaksimFelchuck <felchuck@yandex.ru>
2020-04-15 15:09:02 +0700

    modified link_check

--- lib-utils/link_check.py
+++ lib-utils/link_check.py
@@ -6,6 +6,10 @@ import httplib2
 import requests
 from bs4 import BeautifulSoup
 import lxml
+import time
+import tldextract
+from progress.bar import IncrementalBar
+
 
 class Link:
     def __init__(self, link, status, way):
@@ -26,102 +30,119 @@ _regex = re.compile(
     r'(?:/?|[/?]\S+)$', re.IGNORECASE
 )
 _FORBIDDEN_PREFIXES = ['#', 'tel:', 'mailto:']
-_way = ''
+_host = ''
+_domain = ''
 
-def link_check(url, depth, check_only_internal):
-    global _way
+
+def is_downloadable(url):
+    """
+    Does the url contain a downloadable resource
+    """
+    h = requests.head(url, allow_redirects=True)
+    header = h.headers
+    content_type = header.get('content-type')
+    if 'text' in content_type.lower():
+        return False
+    if 'html' in content_type.lower():
+        return False
+    return True
+
+
+def link_check(url, depth, check_external):
+    global _host, _domain
     if type(url) == str:
-        depth+=1
-        _checked_links.append(url)
-        _way = url
+        ext = tldextract.extract(url)
+        _host = url.split("//")[0] + '//' + ext.domain + '.' + ext.suffix
+        _domain = ext.domain + '.' + ext.suffix
         if depth < 0:
-            return  -1
+            return -1
         elif depth == 0:
             try:
                 request = requests.get(url)
                 status = request.status_code
                 print('Url {0} checked\nstatus code - {1}'.format(url, status))
                 return 0
-            except:
+            except Exception as err:
+                print('Url {0} checked\nError: {1}'.format(url, err))
                 return -1
         else:
-            url = Link(url, None, _way)
+            url = Link(url, None, url)
+            _checked_links.append(url.link)
             main_url = True
     else:
-        _way = _way + '->' + url.link
         main_url = False
-    host = url.link.split("//")[0] + "//" + url.link.split("//")[-1].split("/")[0]
-    domain = url.link.split("//")[-1].split("/")[0]
+
+    if is_downloadable(url.link):
+        request = requests.head(url.link)
+        if request.ok:
+            _valid_links.append(url)
+        else:
+            _invalid_links.append(url)
+        return 0
+
     try:
         request = requests.get(url.link)
         status = request.status_code
         url.status = status
-        if status != 200:
+        if request.ok:
+            _valid_links.append(url)
+        else:
             _invalid_links.append(url)
-            return 0
-        _valid_links.append(url)
+
     except Exception as err:
         url.status = err
         _invalid_links.append(url)
         return 0
-    if depth < 0:
-        _way = _way.split('->')[0]
+    if depth == 0:
         return 0
     soup = BeautifulSoup(request.content, 'lxml')
-    links = _link_search(soup, 'a', 'href', host, domain) \
-     + _link_search(soup, 'link', 'href', host, domain) \
-     + _link_search(soup, 'script', 'src', host, domain) \
-     + _link_search(soup, 'source', 'srcset', host, domain) \
-     + _link_search(soup, 'img', 'src', host, domain) \
-     + _link_search(soup, 'div', 'href', host, domain) \
-     + _link_search(soup, 'h1', 'href', host, domain) \
-     + _link_search(soup, 'h2', 'href', host, domain) \
-     + _link_search(soup, 'h3', 'href', host, domain) \
-     + _link_search(soup, 'h4', 'href', host, domain)
-    internal_links = []
-    external_links = []
-
-    for link in links:
-        if domain not in link:
-            external_links.append(link)
-        else:
-            internal_links.append(link)
-    # check if link must be checked - continue if no
-    for link in internal_links:
-        if link not in _checked_links:
-            _checked_links.append(link)
-            link_check(Link(link, None, _way), depth - 1, check_only_internal)
-    for link in external_links:
-        if link not in _checked_links:
-            _checked_links.append(link)
-            if check_only_internal:
-                link_check(Link(link, None, _way), 0, True)
-            else:
-                link_check(Link(link, None, _way), depth - 1, check_only_internal)
-
+    links = _link_search(soup, 'a', 'href') \
+            + _link_search(soup, 'link', 'href') \
+            + _link_search(soup, 'script', 'src') \
+            + _link_search(soup, 'source', 'srcset') \
+            + _link_search(soup, 'img', 'src', ) \
+            + _link_search(soup, 'div', 'href') \
 
     if main_url:
+        bar = IncrementalBar('Checking: ', max=len(links), suffix='%(percent).1f%% - %(elapsed)ds')
+
+    for link in links:
+        if _host in link:
+            link_check(Link(link, None, url.way + '->' + url.link), depth - 1, check_external)
+        else:
+            if check_external:
+                link_check(Link(link, None, url.way + '->' + url.link), 0, False)
+        if main_url:
+            bar.next()
+
+    if main_url:
+        bar.finish()
         _complete_check(url)
         return status
     else:
         return 0
-def _link_search(soup, tag, attr, host, domain):
+
+
+def _link_search(soup, tag, attr):
     links = []
     for tag in soup.find_all(tag):
         if tag.has_attr(attr):
             link = tag[attr]
             if all(not link.startswith(prefix) for prefix in _FORBIDDEN_PREFIXES):
                 if link.startswith('/') and not link.startswith('//'):
-                    link = host + link
-                if urlparse(link).netloc == domain and link not in _checked_links:
+                    link = _host + link
+                if link not in _checked_links:
                     links.append(link)
+                    _checked_links.append(link)
+
         else:
             pass
     return links
 
+
 def _complete_check(main_link):
-    print(len(_valid_links))
-    print(len(_invalid_links))
+    #print(len(_valid_links))
+    #print(len(_invalid_links))
     if len(_invalid_links) > 0:
         print('Web site: {0} has invalid URLs:'.format(main_link.link))
         for url in _invalid_links:
@@ -151,7 +172,7 @@ def use_as_os_command():
     parser.add_argument('-h', '--help', action='store_true')
     parser.add_argument('url', nargs='?')
     parser.add_argument('depth_to_check', nargs='?', type=int)
-    parser.add_argument('check_internal', nargs='?')
+    parser.add_argument('check_external', nargs='?')
     parser.usage = use_as_os_command.__doc__
     args = parser.parse_args()
     if args.help:
@@ -165,21 +186,21 @@ def use_as_os_command():
         print('''<depth_to_check> isn't specified''')
         print('usage: ' + use_as_os_command.__doc__)
         sys.exit(2)
-    if args.check_internal is None:
+    if args.check_external is None:
         print('''<check_internal> isn't specified''')
         print('usage: ' + use_as_os_command.__doc__)
         sys.exit(2)
-    check_internal = args.check_internal
-    if check_internal == 'False':
-        check_internal = False
-    elif check_internal == 'True':
-        check_internal = True
+    check_external = args.check_external
+    if check_external == 'False':
+        check_external = False
+    elif check_external == 'True':
+        check_external = True
     else:
         print('''<check_external> must be True or False''')
         print('usage: ' + use_as_os_command.__doc__)
         sys.exit(2)
 
-    link_check(args.url, args.depth_to_check, check_internal)
+    link_check(args.url, args.depth_to_check, check_external)
 
 
 if __name__ == '__main__':
